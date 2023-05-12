1b9749ea8d90c3d4b18b5a94460c3eecd21b9e2c
prateeksIngh508 <prateek.singh508@gmail.com>
2023-05-13 03:46:31 +0530

    Added auth

---
+++ dashboard/src/main/java/com/aerothon/dashboard/controllers/AuthenticationController.java
@@ -0,0 +1,45 @@
+package com.aerothon.dashboard.controllers;
+
+import com.aerothon.dashboard.model.User;
+import com.aerothon.dashboard.repositories.UserRepository;
+import com.aerothon.dashboard.util.AuthenticationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthenticationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signUp(@RequestBody User user) {
+
+        String password = user.getPassword();
+        String encryptedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        user.setPassword(encryptedPassword);
+        userRepository.save(user);
+        return ResponseEntity.ok().body("User saved successfully!");
+    }
+
+
+    @PostMapping("/login")
+    public Boolean login(@RequestBody AuthenticationRequest authenticationRequest) {
+        String username = authenticationRequest.getUsername();
+        String password = authenticationRequest.getPassword();
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            return false;
+        }
+        if (BCrypt.checkpw(password, user.getPassword())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
