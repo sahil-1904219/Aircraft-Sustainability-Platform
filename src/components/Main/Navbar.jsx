a2ec94448c171e3c0570d74b4c70566e4b523e1b
Pawan Kumar <pwnsub2001@pec.edu>
2023-05-12 17:58:42 +0530

    added column based on api call

--- src/components/Main/Navbar.jsx
+++ src/components/Main/Navbar.jsx
@@ -1,66 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Nevbar.css";
 
 function Nevbar() {
   const [selectedOption, setSelectedOption] = useState("buy");
+  const [tableData, setTableData] = useState([]);
 
   const handleOptionChange = (option) => {
     setSelectedOption(option);
   };
 
-  // const getTableData = () => {
-  //   // Replace with API call to fetch data based on selectedOption
-  //   switch (selectedOption) {
-  //     case "buy":
-  //       return [
-  //         { id: 1, name: "Component A", price: "$1000" },
-  //         { id: 2, name: "Component B", price: "$500" },
-  //         { id: 3, name: "Component C", price: "$2000" },
-  //       ];
-  //     case "sell":
-  //       return [
-  //         { id: 4, name: "Component D", price: "$1500" },
-  //         { id: 5, name: "Component E", price: "$300" },
-  //         { id: 6, name: "Component F", price: "$2500" },
-  //       ];
-  //     case "recycle":
-  //       return [
-  //         { id: 7, name: "Component G", weight: "100 kg" },
-  //         { id: 8, name: "Component H", weight: "50 kg" },
-  //         { id: 9, name: "Component I", weight: "200 kg" },
-  //       ];
-  //     default:
-  //       return [];
-  //   }
-  // };
-  const getTableData = async () => {
-    try {
-      let response;
-      switch (selectedOption) {
-        case "buy":
-          response = await fetch("https://example.com/buy");
-          break;
-        case "sell":
-          response = await fetch("https://example.com/sell");
-          break;
-        case "recycle":
-          response = await fetch("https://example.com/recycle");
-          break;
-        default:
-          return [];
+  useEffect(() => {
+    const getTableData = async () => {
+      try {
+        let response;
+        switch (selectedOption) {
+          case "buy":
+            response = await fetch("http://localhost:8080/parts/newParts");
+            break;
+          case "sell":
+            response = await fetch("http://localhost:8080/parts/oldParts");
+            break;
+          case "recycle":
+            response = await fetch("http://localhost:8080/parts/recycle");
+            break;
+          default:
+            return [];
+        }
+        if (response.ok) {
+          const data = await response.json();
+          setTableData(data);
+        } else {
+          throw new Error("API call failed");
+        }
+      } catch (error) {
+        console.error(error);
+        setTableData([]);
       }
-      if (response.ok) {
-        const data = await response.json();
-        return data;
-      } else {
-        throw new Error("API call failed");
-      }
-    } catch (error) {
-      console.error(error);
-      return [];
+    };
+    getTableData();
+  }, [selectedOption]);
+
+  const getColumns = () => {
+    switch (selectedOption) {
+      case "buy":
+        return ["id", "name", "buyprice"];
+      case "sell":
+        return ["id", "name", "sellPrice"];
+      case "recycle":
+        return ["id", "name", "weight"];
+      default:
+        return [];
     }
   };
-
 
   return (
     <div className="Nevbar">
@@ -71,10 +62,8 @@ function Nevbar() {
             width: "33.33%",
             padding: "10px 12px",
           }}
-
           className={selectedOption === "buy" ? "active" : ""}
           onClick={() => handleOptionChange("buy")}
-
         >
           Buy
         </button>
@@ -105,27 +94,17 @@ function Nevbar() {
       <table>
         <thead>
           <tr>
-            {selectedOption === "buy" || selectedOption === "sell" ? (
-              <>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Price</th>
-              </>
-            ) : (
-              <>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Weight</th>
-              </>
-            )}
+            {getColumns().map((column) => (
+              <th key={column}>{column.toUpperCase()}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {getTableData().map((item) => (
+          {tableData.map((item) => (
             <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{selectedOption === "buy" || selectedOption === "sell" ? item.price : item.weight}</td>
+              {getColumns().map((column) => (
+                <td key={column}>{item[column]}</td>
+              ))}
             </tr>
           ))}
         </tbody>
