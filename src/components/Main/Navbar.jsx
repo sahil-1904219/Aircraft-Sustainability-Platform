6becbecef76b0756886ea7644d3b34c8de3fa25a
Pawan Kumar <pwnsub2001@pec.edu>
2023-05-12 16:22:17 +0530

    api call and take response in json

--- src/components/Main/Navbar.jsx
+++ src/components/Main/Navbar.jsx
@@ -8,31 +8,59 @@ function Nevbar() {
     setSelectedOption(option);
   };
 
-  const getTableData = () => {
-    // Replace with API call to fetch data based on selectedOption
-    switch (selectedOption) {
-      case "buy":
-        return [
-          { id: 1, name: "Component A", price: "$1000" },
-          { id: 2, name: "Component B", price: "$500" },
-          { id: 3, name: "Component C", price: "$2000" },
-        ];
-      case "sell":
-        return [
-          { id: 4, name: "Component D", price: "$1500" },
-          { id: 5, name: "Component E", price: "$300" },
-          { id: 6, name: "Component F", price: "$2500" },
-        ];
-      case "recycle":
-        return [
-          { id: 7, name: "Component G", weight: "100 kg" },
-          { id: 8, name: "Component H", weight: "50 kg" },
-          { id: 9, name: "Component I", weight: "200 kg" },
-        ];
-      default:
-        return [];
+  // const getTableData = () => {
+  //   // Replace with API call to fetch data based on selectedOption
+  //   switch (selectedOption) {
+  //     case "buy":
+  //       return [
+  //         { id: 1, name: "Component A", price: "$1000" },
+  //         { id: 2, name: "Component B", price: "$500" },
+  //         { id: 3, name: "Component C", price: "$2000" },
+  //       ];
+  //     case "sell":
+  //       return [
+  //         { id: 4, name: "Component D", price: "$1500" },
+  //         { id: 5, name: "Component E", price: "$300" },
+  //         { id: 6, name: "Component F", price: "$2500" },
+  //       ];
+  //     case "recycle":
+  //       return [
+  //         { id: 7, name: "Component G", weight: "100 kg" },
+  //         { id: 8, name: "Component H", weight: "50 kg" },
+  //         { id: 9, name: "Component I", weight: "200 kg" },
+  //       ];
+  //     default:
+  //       return [];
+  //   }
+  // };
+  const getTableData = async () => {
+    try {
+      let response;
+      switch (selectedOption) {
+        case "buy":
+          response = await fetch("https://example.com/buy");
+          break;
+        case "sell":
+          response = await fetch("https://example.com/sell");
+          break;
+        case "recycle":
+          response = await fetch("https://example.com/recycle");
+          break;
+        default:
+          return [];
+      }
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error("API call failed");
+      }
+    } catch (error) {
+      console.error(error);
+      return [];
     }
   };
+
 
   return (
     <div className="Nevbar">
